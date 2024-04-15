c78b6972def646d8f71f29743bbf4bca77097d0c
JoseGlez <A01028517@tec.mx>
2024-04-15 14:44:39 -0600

    Comentario

--- operations.py
+++ operations.py
@@ -1,6 +1,9 @@
+
 a = int(input("Give me the first number: "))
 b = int(input("Give me the second number: "))
 
 print("Addition: " a + b)
 print("Subtraction: " a - b)
 print("Multiplication: " a * b)
+
+#Display the multiplication (jose)
